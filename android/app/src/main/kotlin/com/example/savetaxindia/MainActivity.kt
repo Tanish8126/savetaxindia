b896899b93280996161cae467a57f0e37618956a
Tanish8126 <tanishpurwar37@gmail.com>
2025-06-11 18:01:58 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/savetaxindia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.savetaxindia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/taxcal/taxcal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Taxcal extends StatelessWidget {
+  const Taxcal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(body: Text("Helooooooooooooo")));
+  }
+}
